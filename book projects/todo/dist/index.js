71d2c50aae6139d166509675675b5d77574443ac
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-07-27 17:42:43 +0500

    todo with edit todo item

--- book projects/todo/dist/index.js
+++ book projects/todo/dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoList_1 = require("./todoList");
 let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"), new todoItem_1.TodoItem(2, "Get Shoes"),
     new todoItem_1.TodoItem(3, "Collect Tickets"), new todoItem_1.TodoItem(4, "Call Joe", true)
 ];
 let collection = new todoList_1.TodoList("Adam", todos);
