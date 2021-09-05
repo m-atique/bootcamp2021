56c24b0652334d0da1d73a655d9cfd085b81d5b0
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-09-05 08:07:18 +0500

    Printing grpaghql api url

--- Class code/graphql/lib/graphql-stack.ts
+++ Class code/graphql/lib/graphql-stack.ts
@@ -9,18 +9,24 @@ export class GraphqlStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     //----------------------------------------------
-    const api = new appsync.GraphqlApi(this, 'GraphqlApi',{
-      name:'cdk-api',
-      schema:appsync.Schema.fromAsset('Gql/schema.gql'), //----------path specified for lambda 
-      authorizationConfig:{                              //----------defining autherization
-        defaultAuthorization:{
-          authorizationType:appsync.AuthorizationType.API_KEY,
-          apiKeyConfig:{
-            expires:cdk.Expiration.after(cdk.Duration.days(365))   //-----------defining expiy
+    const api = new appsync.GraphqlApi(this, 'GraphqlApi', {
+      name: 'cdk-api',
+      schema: appsync.Schema.fromAsset('Gql/schema.gql'), //----------path specified for lambda 
+      authorizationConfig: {                              //----------defining autherization
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365))   //-----------defining expiy
+          }
         }
-      }
       },
-      xrayEnabled:true  //--------------- enablaing xray debbug(an aws code debbugear)
+      xrayEnabled: true  //--------------- enablaing xray debbug(an aws code debbugear)
     })
+
+//---------------------printing Grpaph ql url after deploying
+    new cdk.CfnOutput(this, 'GrpahqlApiUrl', {
+      value: api.graphqlUrl
+    })
+
   }
 }
