6c0d1521095e30d3ad162b01fa176ea613625152
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-09-05 08:00:40 +0500

    difine const api in grpahqlStack

--- Class code/graphql/lib/graphql-stack.ts
+++ Class code/graphql/lib/graphql-stack.ts
@@ -7,5 +7,20 @@ export class GraphqlStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    //----------------------------------------------
+    const api = new appsync.GraphqlApi(this, 'GraphqlApi',{
+      name:'cdk-api',
+      schema:appsync.Schema.fromAsset('Gql/schema.gql'), //----------path specified for lambda 
+      authorizationConfig:{                              //----------defining autherization
+        defaultAuthorization:{
+          authorizationType:appsync.AuthorizationType.API_KEY,
+          apiKeyConfig:{
+            expires:cdk.Expiration.after(cdk.Duration.days(365))   //-----------defining expiy
+        }
+      }
+      },
+      xrayEnabled:true  //--------------- enablaing xray debbug(an aws code debbugear)
+    })
   }
 }
