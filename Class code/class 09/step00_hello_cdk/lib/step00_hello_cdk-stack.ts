db2748c6c345e3e067ba6808c993298ca7d5198e
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-12-11 18:29:08 +0500

    lamba  and gateway coded

--- Class code/class 09/step00_hello_cdk/lib/step00_hello_cdk-stack.ts
+++ Class code/class 09/step00_hello_cdk/lib/step00_hello_cdk-stack.ts
@@ -1,9 +1,31 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
+// --------------------------------------------importing s3 bucket
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class Step00HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // new s3.Bucket(this,'myFirstBucket',{
+    //   versioned:true
+    // })
+
+    //-----------------------------------------creating lambda 
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
+
+  //-------------------------------------------apigateway
+  new apigw.LambdaRestApi(this, "Endpoint", {
+    handler: hello,
+  });
+
+
   }
 }
