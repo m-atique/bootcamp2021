da3917fdfa3288b889f636b36dda17537659eaee
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2021-12-13 13:22:19 +0500

    intiated new project for graphql

---
+++ Class code/class 11/hellographql/lib/hellographql-stack.ts
@@ -0,0 +1,14 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as appsync from '@aws-cdk/aws-appsync'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HellographqlStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    
+  }
+}
---
+++ Class code/class 11/hellographql/test/hellographql.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hellographql from '../lib/hellographql-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hellographql-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hellographql.HellographqlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
